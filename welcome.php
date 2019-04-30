c32daf37a47b2580f40fbfedaa26afdcdcb0868c
EanW <50156020+EanW@users.noreply.github.com>
2019-04-30 16:18:45 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+	
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+The date given is:	<?php echo date("Y-m-d");?>
+
+</body>
+</html>
